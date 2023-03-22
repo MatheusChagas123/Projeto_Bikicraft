79b46f31e27c682c457ed91c5152404c05e2cfca
Matheus Chagas <matheuschagas29@gmail.com>
2023-03-22 15:16:56 -0300

    js perguntas

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//ativar links do menu
+
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -8,3 +10,33 @@ function ativarLink(link) {
   }
 }
 links.forEach(ativarLink);
+
+// ativar items orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+  resposta.classList.toggle("ativa");
+  console.log(resposta);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
